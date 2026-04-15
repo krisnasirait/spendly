eda9e7f8ac5a73816907cce9747a5e5b18cb1d9d
Krisna Sirait <krisnasirait48@gmail.com>
2026-04-16 01:28:22 +0700

    feat: apply gen-z UI revamp - light & playful design

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,10 +13,6 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"
-          rel="stylesheet"
-        />
       </head>
       <body>
         <Providers>{children}</Providers>
